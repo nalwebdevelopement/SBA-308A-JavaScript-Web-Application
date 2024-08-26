75e5f3f107fadb1c4da7fe2cec89b6a689f1476c
nalwebdevelopement <naliniu91@gmail.com>
2024-08-25 20:13:15 -0400

    completed Currency converter

--- app.mjs
+++ app.mjs
@@ -3,6 +3,7 @@ let jsonData1
 let s1;
 let s2;
 let cur1,cur2
+
 let amt1 = document.getElementById('amtbox1');
 let amt2 = document.getElementById('amtbox2');
 
@@ -59,17 +60,59 @@ function getdetails()
     //const s11 = document.getElementById("sbox1");
     cur1 = s1.value;
     cur2 = s2.value;
-    inputvalue = amt1.value;
+    let inputvalue = amt1.value;
     if(cur1===cur2)
     {
         alert("choose different country");
+       // document.getElementById('error').innerHTML = "choose different country";
+
     }
     else
     {
         convert(cur1,cur2,inputvalue)
     }
 }
-function convert(cur1,cur2,inputvalue)
+// function convert(cur1,cur2,inputvalue)
+// {
+//     const host = 'api.frankfurter.app';
+//    fetch(`https://${host}/latest?amount=${inputvalue}&from=${cur1}&to=${cur2}`)
+//   .then(resp => resp.json())
+//   .then((data) => {
+//     //alert(`10 GBP = ${data.rates.USD} USD`);
+//     console.log(data);
+//     //console.log(Object.values(data.rates));
+//     const result = Object.values(data.rates);
+//    // console.log(result);
+//     amt2.value = result;
+//     document.getElementById('result').innerHTML = result;
+//   });
+// }
+
+
+
+async function convert(cur1,cur2,inputvalue)
 {
+    const host = 'api.frankfurter.app';
+    try{
     
+        const response = await fetch(`https://${host}/latest?amount=${inputvalue}&from=${cur1}&to=${cur2}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+    
+   
+   
+    console.log(data);
+    //console.log(Object.values(data.rates));
+    const result = Object.values(data.rates);
+   // console.log(result);
+    amt2.value = result;
+    document.getElementById('result').innerHTML = result;
+   }
+   catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+   }
+  
 }
